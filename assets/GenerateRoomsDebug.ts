89d5526439db295334ba350e7cd52495dc581506
sanya1111111111 <116189775+sanya1111111111@users.noreply.github.com>
2024-02-29 13:19:11 +0300

    Merge

--- assets/GenerateRoomsDebug.ts
+++ assets/GenerateRoomsDebug.ts
@@ -4,7 +4,9 @@ const { ccclass, property } = _decorator;
 @ccclass('GenerateRoomsDebug')
 export class GenerateRoomsDebug extends Component {
     
-    private rooms: string[] = ['rooms/room_test_papka', 'rooms/room_test_shop', 'rooms/room_test_1',];
+    private rooms: string[] = ['rooms/room_test_papka', 'rooms/room_test_shop', 'rooms/room_test_1'];
+    private roomExits: number[][] = [];
+    private roomList: Node[] = [];
     
     onLoad() {
         console.log(this.rooms);
@@ -14,48 +16,56 @@ export class GenerateRoomsDebug extends Component {
         
     }
 
-    createRoom(p: string, X: number): number{
+    createRoom(p: string) {
         let a: any;
         let b: any;
         let dirList: number[] = [NaN, NaN, NaN, NaN, NaN, NaN, NaN, NaN]
         resources.load(p, Prefab, (err, prefab) => {
-            
             const newNode = instantiate(prefab);
             director.getScene().getChildByName('Canvas').addChild(newNode);
-            newNode.setPosition(X, 0);
+            //newNode.setPosition(X, 0);
             a = newNode.getChildByName('Door_left');
             b = newNode.getChildByName('Door_right');
+            this.roomList.push(newNode);
             if (a) {
-                dirList[0] = (a.position.x + a.width/2);
-                dirList[1] = (a.position.y + a.height/2);
+                dirList[0] = (a.position.x + a.width / 2);
+                dirList[1] = (a.position.y + a.height / 2);
                 //console.log(dirList)
             }
             if (b) {
-                dirList[2] = (b.position.x + b.width/2);
-                dirList[3] = (b.position.y + b.height/2);
+                dirList[2] = (b.position.x + b.width / 2);
+                dirList[3] = (b.position.y + b.height / 2);
                 //console.log(dirList)
             }
-            
-            
+            this.roomExits.push(dirList);
+            //console.log(this.roomExits);
         })
-        console.log(dirList)
         //console.log(dirList+" room " + p + " added at" + X);
-        return X+100
+        //return X + 100
     }
+
+    
+
     start() {
-        
         this.rooms = this.rooms.sort(() => Math.random() - 0.5);
         let pos: number = 0;
         while (this.rooms.length  > 0) {
-            pos = this.createRoom(this.rooms.pop(), pos);
+            this.createRoom(this.rooms.pop());
             //pos += 100;
         }
+        director.getScene().getChildByName
+        setTimeout(() => {
+            this.roomList = this.roomList.sort(() => Math.random() - 0.5);
+
+            for (let i in this.roomList) {
+                //this.roomList[i].position = ;
+            }
+            console.log(this.roomExits)
+            
+        }, 1000)
         
-       /*
-        resources.load('rooms/room_test_papka', Prefab, (err, prefab) => {
-            const newNode = instantiate(prefab);
-            director.getScene().getChildByName('Canvas').addChild(newNode);
-        });*/
+        
+        
     }
 
     update(deltaTime: number) {
--- assets/scripts/Player/Player.ts
+++ assets/scripts/Player/Player.ts
@@ -1,4 +1,4 @@
-    import { _decorator, Component, Node, systemEvent, SystemEventType, EventKeyboard, macro, Vec2, RigidBody2D, Collider2D, BoxCollider2D, Contact2DType, IPhysics2DContact, Label, ProgressBar, RaycastResult2D, PhysicsRayResult, Director, Input, KeyCode, XrKeyboardEventType} from 'cc';
+import { _decorator, Component, Node, systemEvent, SystemEventType, EventKeyboard, macro, Vec2, RigidBody2D, Collider2D, BoxCollider2D, Contact2DType, IPhysics2DContact, Label, Prefab, director, instantiate, DistanceJoint2D, error, RigidBodyComponent, ERigidBody2DType, EventMouse } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
@@ -6,6 +6,14 @@ export class Player extends Component {
     @property({ type: Label })
     private HPLabel: Label | null = null;
     private HP: number = 100;
+    @property(Prefab)
+    segmentPrefab: Prefab = null;
+
+    @property(Prefab)
+    hookPrefab: Prefab = null;
+
+    @property
+    ropeLength: number = 10;
 
     private collider: any;
     private rigidbody: any;
@@ -14,11 +22,107 @@ export class Player extends Component {
     private jump_force: number = 40;
     private _startJump: boolean = false;
 
+    private rope: Node[] = [];
+    private hook: Node;
+    private hContact: boolean = false;
+    private hLaunch: boolean = false;
+    
 
     onLoad() {
         systemEvent.on(SystemEventType.KEY_DOWN, this.onKeyRightLeft, this);
         systemEvent.on(SystemEventType.KEY_UP, this.onKeyUp, this);
+        director.getScene().getChildByName("Canvas").getChildByName("WallOutside").on(Node.EventType.MOUSE_DOWN, (event: EventMouse) => {
+            if (event.getButton() == 2) {
+                this.hookLaunch(event.getLocation());
+            } //console.log();
+        }, this);
     }
+    hookJump() {
+
+    }
+    hookLaunch(mouseLoc: Vec2) {
+        if (this.hContact) this.hookDespawn();
+        if (this.hLaunch) return;
+        this.hLaunch = true;
+        console.log(mouseLoc);
+        console.log(this.node.position);
+        let prevSegment = this.node;
+        let sc = director.getScene().getChildByName("Canvas");
+        
+        for (let i = 0; i < this.ropeLength; i++) {
+            let segment = instantiate(this.segmentPrefab);
+            
+            sc.addChild(segment);
+            segment.setPosition(prevSegment.position.x, prevSegment.position.y);
+
+            let distanceJoint = segment.getComponent(DistanceJoint2D);
+
+            if (distanceJoint) {
+                distanceJoint.connectedBody = prevSegment.getComponent(RigidBody2D);
+                distanceJoint.enabled = false; //Удаление этой строки ломает всю физику
+                distanceJoint.enabled = true;
+            } else {
+                error("DistanceJoint component not found on segment prefab.");
+            }
+            //console.log(this.rope);
+            this.rope.push(segment);
+            prevSegment = segment;
+        }
+        
+        this.hook = instantiate(this.hookPrefab);
+        sc.addChild(this.hook);
+        this.hook.setPosition(prevSegment.position.x, prevSegment.position.y);
+
+        this.hook.getComponent(Collider2D).on(Contact2DType.BEGIN_CONTACT, this.hookHit, this);
+
+        let distanceJoint = this.hook.getComponent(DistanceJoint2D);
+        distanceJoint.connectedBody = this.rope[this.ropeLength - 1].getComponent(RigidBody2D);
+        distanceJoint.enabled = false; //Удаление этой строки ломает всю физику
+        distanceJoint.enabled = true; //Удаление этой строки ломает всю физику
+        //let x = 0;
+        //if (mouseLoc.x - this.node.worldPosition.x < 0)
+        //console.log(x)
+        let a = new Vec2;
+        let b = new Vec2(this.node.worldPosition.x, this.node.worldPosition.y);
+        a = mouseLoc.subtract(b);
+        console.log(a);
+        let modul: number = (Math.abs(this.node.worldPosition.x) * Math.abs(this.node.worldPosition.y)) / (Math.abs(a.x) * Math.abs(a.y));
+        //a.set(a.x * modul, a.y * modul);
+        console.log(a);
+        //console.log(mouseLoc.signAngle(a));
+        //(/*(new Vec2(this.node.position.x, this.node.position.y)).angle(mouseLoc)*/);
+        this.hook.getComponent(RigidBody2D).linearVelocity = a;
+        //mouseLoc.x - this.node.worldPosition.x, mouseLoc.y - this.node.worldPosition.y); //applyForceToCenter(new Vec2(mouseLoc.x - this.node.worldPosition.x, mouseLoc.y - this.node.worldPosition.y), true);
+        //setTimeout(this.hookDespawn, 10000);
+    }
+
+    hookHit(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null) {
+        if (otherCollider.name != "ropeSegment1<BoxCollider2D>" && !this.hContact && otherCollider.node != this.node) {
+            this.hContact = true;
+            console.log(otherCollider.node);
+            this.hook.getComponent(RigidBody2D).linearVelocity = new Vec2(0, 0);
+            for (let i in this.rope) {
+                //console.log(i);
+                
+            }
+        }
+    }
+
+    hookDespawn() {
+        this.hook.removeFromParent();
+        this.hook.destroy();
+        let c: Node;
+        for (let i = 0; i < this.ropeLength; i++) {
+            c = this.rope.pop();
+            console.log(i);
+            c.removeFromParent();
+            c.destroy();
+        }
+        this.hContact = false;
+        this.hLaunch = false;
+    }
+
+    
 
     start() {
         this.rigidbody = this.node.getComponent(RigidBody2D);
@@ -32,7 +136,9 @@ export class Player extends Component {
 
     update(deltaTime: number) {
         this.rigidbody.applyForceToCenter(new Vec2(this.direction * this.walk_force, 0), true);
+        if (this.hook && !this.hContact) {
 
+        }
     }
 
 
@@ -61,7 +167,6 @@ export class Player extends Component {
                 } else {
                     console.log("intheair");
                 }
-
                 break;
             default:
                 break;
